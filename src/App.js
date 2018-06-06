af7df9e75f1828695c03e0b50b168ba6e901503a
Brad Hain <bradhain@gmail.com>
2018-06-06 17:05:03 -0600

    first push

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import NameBadge from './NameBadge'
+import './App.css';
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div className="App">
+        <NameBadge name='Brad'/>
+
+      </div>
+      
+    );
+  }
+}
+export default App;
---
+++ src/NameBadge.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+class NameBadge extends React.Component{
+  render(){
+
+
+
+    return(
+      <div>
+        <h1>{this.props.name}</h1>
+      </div>
+    )
+  }
+}
+
+
+
+export default NameBadge
